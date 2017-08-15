daf5f5e1b6d41971e323d85bb09f8940b7a4ec39
lain <horozhe@gmail.com>
2017-08-15 06:14:32 +0300

    With not working file in post body

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"os"
+	"log"
 )
 
 func main() {
+	buf, file_err := os.Open(os.Args[1])
+	if file_err != nil{
+		log.Fatal("input error")
+	}
 	resp, err := http.Post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyD8AT_TbaiOotFmgMsdHZ1lifx1bIGQo-Q", "image/jpeg", &buf)
 	if err!= nil {
 		fmt.Println(resp)
-	} else { fmt.Println(err)
+	} else { log.Fatal(err)
 	}
 }
