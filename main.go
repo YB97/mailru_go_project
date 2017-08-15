c099d1a9bc87fb7ef5e9f1f87b37a7e52a3d2271
lain <horozhe@gmail.com>
2017-08-15 05:44:02 +0300

    with api key

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	resp, err := http.Post("https://vision.googleapis.com/v1/images:annotate?key=api_key_would_be_here", "image/jpeg", &buf)
+	resp, err := http.Post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyD8AT_TbaiOotFmgMsdHZ1lifx1bIGQo-Q", "image/jpeg", &buf)
 	if err!= nil {
 		fmt.Println(resp)
 	} else { fmt.Println(err)
