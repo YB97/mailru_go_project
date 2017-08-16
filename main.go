5e54e64fe23654ea465f1240bc27e8df21387308
lain <horozhe@gmail.com>
2017-08-16 18:35:51 +0300

    with 403 error

--- main.go
+++ main.go
@@ -5,16 +5,33 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"bytes"
+	"mime/multipart"
 )
 
 func main() {
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
 	buf, file_err := os.Open(os.Args[1])
 	if file_err != nil{
 		log.Fatal("input error")
 	}
-	resp, err := http.Post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyD8AT_TbaiOotFmgMsdHZ1lifx1bIGQo-Q", "image/jpeg", &buf)
-	if err!= nil {
-		fmt.Println(resp)
-	} else { log.Fatal(err)
+	defer buf.Close()
+	fw, err := w.CreateFormFile("image", os.Args[1])
+	if fw, err = w.CreateFormField("key"); err != nil {
+		fmt.Println(fw)
 	}
+	req, err := http.NewRequest("POST", "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA9QNmSSQNO0JF_HSQUnQqdqRTR6YWYyBo", &b)
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(res)
 }
