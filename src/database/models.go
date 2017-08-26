d175b02bc9d82c1245075ac08767d91767fab1e9
lain <horozhe@gmail.com>
2017-08-26 08:36:09 +0300

    Revert "models fix"
    This reverts commit 1c89e6401e7ef7a9f911e15feb40bc967009808e.

--- src/database/models.go
+++ src/database/models.go
@@ -1,25 +1,22 @@
 package database
 
 type User struct {
-	ID       int     `gorm:"primary_key; AUTO_INCREMENT"`
-	LOGIN    string  `gorm:"size:255; unique; not null"`
-	PASSWORD string  `gorm:"size:255; not null"`
-	UUID     string  `gorm:"size:255"`
-	Image    []Image `gorm:"ForeignKey:ImageID"`
-	ImageID  uint
+	ID       int    `gorm:"primary_key; AUTO_INCREMENT"`
+	LOGIN    string `gorm:"size:255; unique; not null"`
+	PASSWORD string `gorm:"size:255; not null"`
+	UUID     string `gorm:"size:255"`
 }
 
 type Image struct {
 	ID       int    `gorm:"primary_key"`
 	FILENAME string `gorm:"size:255"`
-	User     User   `gorm:"ForeignKey:UserID"`
-	UserID   uint
+	LABEL    string `gorm:"size:600"`
 }
 
 type Queue struct {
-	ID      int   `gorm:"primary_key"`
-	User    User  `gorm:"ForeignKey:UserID"`
-	Image   Image `gorm:"ForeignKey:ImageID"`
-	UserID  uint
+	ID      int     `gorm:"primary_key"`
+	User User `gorm:"ForeignKey:UserID"`
+	Image Image `gorm:"ForeignKey:ImageID"`
+	UserID uint
 	ImageID uint
 }
