1c89e6401e7ef7a9f911e15feb40bc967009808e
lain <horozhe@gmail.com>
2017-08-26 07:26:18 +0300

    models fix

--- src/database/models.go
+++ src/database/models.go
@@ -1,22 +1,25 @@
 package database
 
 type User struct {
-	ID       int    `gorm:"primary_key; AUTO_INCREMENT"`
-	LOGIN    string `gorm:"size:255; unique; not null"`
-	PASSWORD string `gorm:"size:255; not null"`
-	UUID     string `gorm:"size:255"`
+	ID       int     `gorm:"primary_key; AUTO_INCREMENT"`
+	LOGIN    string  `gorm:"size:255; unique; not null"`
+	PASSWORD string  `gorm:"size:255; not null"`
+	UUID     string  `gorm:"size:255"`
+	Image    []Image `gorm:"ForeignKey:ImageID"`
+	ImageID  uint
 }
 
 type Image struct {
 	ID       int    `gorm:"primary_key"`
 	FILENAME string `gorm:"size:255"`
-	LABEL    string `gorm:"size:600"`
+	User     User   `gorm:"ForeignKey:UserID"`
+	UserID   uint
 }
 
 type Queue struct {
-	ID      int     `gorm:"primary_key"`
-	User User `gorm:"ForeignKey:UserID"`
-	Image Image `gorm:"ForeignKey:ImageID"`
-	UserID uint
+	ID      int   `gorm:"primary_key"`
+	User    User  `gorm:"ForeignKey:UserID"`
+	Image   Image `gorm:"ForeignKey:ImageID"`
+	UserID  uint
 	ImageID uint
 }
